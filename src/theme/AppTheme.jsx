f25d84e513a1ac747a4c46baf6cca45e89114335
javier <javc2804@gmail.com>
2024-02-21 09:52:30 -0400

    Configurado MUI y estructura de archivos

---
+++ src/theme/AppTheme.jsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { purpleTheme } from "./purpleTheme";
+
+const AppTheme = ({ children }) => {
+  return (
+    <ThemeProvider theme={purpleTheme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export default AppTheme;
