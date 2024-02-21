2c386cd20819660d2834aba81a39f06828ed6c54
javier <javc2804@gmail.com>
2024-02-21 11:41:16 -0400

    Add AuthLayout component and update LoginPage and RegisterPage

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,7 +1,45 @@
+import { Link as RouterLink } from "react-router-dom";
+import { Button, Grid, TextField, Typography, Link } from "@mui/material";
+import AuthLayout from "../layout/AuthLayout";
+
 export const RegisterPage = () => {
   return (
     <>
-      <h1>Register</h1>
+      <AuthLayout title={"Register"}>
+        <form>
+          <Grid container>
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label="Nombre Completo"
+                type="text"
+                placeholder="Nombre completo"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label="Clave"
+                type="password"
+                placeholder="Clave"
+                fullWidth
+              />
+            </Grid>
+            <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+              <Grid item xs={12}>
+                <Button variant="contained" fullWidth>
+                  Crear cuenta
+                </Button>
+              </Grid>
+            </Grid>
+            <Grid container direction="row" justifyContent="end">
+              <Typography sx={{ mr: 1 }}>¿Ya tienes una cuenta?</Typography>
+              <Link component={RouterLink} color="inherit" to="/auth/login">
+                Ingresar
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </AuthLayout>
     </>
   );
 };
